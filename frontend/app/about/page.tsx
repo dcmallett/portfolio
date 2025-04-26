e519502159e8d567116fe370c5d84f066562431e
dcmallett <dcmallett28@gmail.com>
2025-04-26 15:11:21 +0100

    intial files sorting

---
+++ frontend/app/about/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const about = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>This is the about page of my portfolio.</p>
+      <p>Here you can find information about me and my work.</p>
+      <p>Feel free to reach out if you have any questions!</p>
+    </div>
+  )
+}
+
+export default about
